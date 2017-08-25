dc85dfbbbaefd4e65142aacdfa842f6a03914d6f
Eric Bastarache <eric_bastarache@hotmail.com>
2017-08-25 13:44:50 -0400

    Initial project commit

---
+++ src/components/About.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bordered">
+    <h1>{{aboutText}}</h1>
+    <a href="#/">Back To Home</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      aboutText: 'This is About',
+    };
+  },
+};
+</script>
+<style scoped>
+h1 {
+  color: #ff0000;
+}
+
+div.bordered {
+  border: 1px solid #0000ff;
+}
+</style>
