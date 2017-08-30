c0dcbf7fa6d291e59688285fea184bca203e8804
Eric Bastarache <eric_bastarache@hotmail.com>
2017-08-29 22:52:19 -0400

    Created Data component

---
+++ src/components/Data.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+      <div v-for="item in items" v-bind:key="item.id">
+          <h1>{{item.id}}</h1>
+          <p>{{item.name}}</p>
+      </div>
+  </div>
+</template>
+<script>
+  export default {
+    data() {
+      return {
+        items: [],
+      };
+    },
+    created() {
+      this.items = [
+          { id: 1, name: 'Bob' },
+          { id: 2, name: 'Joe' },
+          { id: 3, name: 'Mack' },
+          { id: 4, name: 'John' },
+          { id: 5, name: 'Alyssa' },
+      ];
+    },
+  };
+</script>
