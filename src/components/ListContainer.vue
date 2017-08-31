5b4dfca6d666a5835da4c31a61388d1e039f4e1f
Eric Bastarache <eric_bastarache@hotmail.com>
2017-08-30 21:48:17 -0400

    Created container component for list

---
+++ src/components/ListContainer.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="list-holder">
+      <list></list>
+  </div>
+</template>
+
+<script>
+  import List from './List';
+
+  export default {
+    components: {
+      list: List,
+    },
+    data() {
+      return {
+        listItems: [],
+      };
+    },
+  };
+</script>
