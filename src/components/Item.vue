8654aaf0a2af9b83f009e6b9387e54330dfddac6
Eric Bastarache <eric_bastarache@hotmail.com>
2017-08-30 21:48:50 -0400

    Created item component for list

---
+++ src/components/Item.vue
@@ -0,0 +1,15 @@
+<template>
+  <li>
+    <a href="#">{{item}}</a>
+  </li>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        item: 'Item Text',
+      };
+    },
+  };
+</script>
