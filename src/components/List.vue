78caeb5ffa08fc441d7df23105e3ce62770d971b
Eric Bastarache <eric_bastarache@hotmail.com>
2017-08-30 21:48:35 -0400

    Created list component for holding list data

---
+++ src/components/List.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+  <ul>
+    <item v-for="item in items" v-bind:key="item.id"></item>
+  </ul>
+  <button @click="addItem()">Add New Item</button>
+</div>
+</template>
+<script>
+  import Item from './Item';
+
+  export default {
+    components: {
+      item: Item,
+    },
+    data() {
+      return {
+        items: [],
+      };
+    },
+    methods: {
+      getItems() {
+        return this.items;
+      },
+      addItem() {
+        return this.items.push({ id: this.items.length, name: 'Test' });
+      },
+    },
+  };
+</script>
